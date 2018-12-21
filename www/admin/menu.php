2fda89543216d51cfdf040e4aaefc14c2a7938fa
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2018-12-21 19:28:14 +0100

    add products to admin page

--- www/admin/menu.php
+++ www/admin/menu.php
@@ -1,6 +1,7 @@
 <h1>Admin</h1>
 <ul id='adminmenu'>
     <li><a href='index.php'>Admin</a></li>
+    <li><a href='products.php'>Products</a></li>
     <li><a href='users.php'>Users</a></li>
     <li><a href="../index.php">View Website</a></li>
     <li><a href='logout.php'>Logout</a></li>
--- www/admin/products.php
+++ www/admin/products.php
@@ -6,18 +6,13 @@ require_once('../includes/config.php');
 if(!$user->is_logged_in()){ header('Location: login.php'); }
 
 //show message from add / edit page
-if(isset($_GET['deluser'])){
+if(isset($_GET['delproduct'])){
 
-    //if user id is 1 ignore
-    if($_GET['deluser'] !='1'){
+    $stmt = $db->prepare('DELETE FROM butcher_shop_products WHERE productID = :productID') ;
+    $stmt->execute(array(':productID' => $_GET['delproduct']));
 
-        $stmt = $db->prepare('DELETE FROM butcher_shop_members WHERE memberID = :memberID') ;
-        $stmt->execute(array(':memberID' => $_GET['deluser']));
-
-        header('Location: users.php?action=deleted');
-        exit;
-
-    }
+    header('Location: products.php?action=deleted');
+    exit;
 }
 
 ?>
@@ -25,15 +20,15 @@ if(isset($_GET['deluser'])){
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Admin - Users</title>
+    <title>Admin - products</title>
     <link rel="stylesheet" href="../style/normalize.css">
     <link rel="stylesheet" href="../style/main.css">
     <script language="JavaScript" type="text/javascript">
-    function deluser(id, title)
+    function delproduct(id, title)
     {
         if (confirm("Are you sure you want to delete '" + title + "'"))
         {
-            window.location.href = 'users.php?deluser=' + id;
+            window.location.href = 'products.php?delproduct=' + id;
         }
     }
     </script>
@@ -44,31 +39,26 @@ if(isset($_GET['deluser'])){
         <?php
         //show message from add / edit page
         if(isset($_GET['action'])){
-            echo '<h3>User '.$_GET['action'].'.</h3>';
+            echo '<h3>Product '.$_GET['action'].'.</h3>';
         }
         ?>
         <table>
             <tr>
-                <th>Username</th>
-                <th>Email</th>
+                <th>Title</th>
                 <th>Action</th>
             </tr>
             <?php
             try {
-                $stmt = $db->query('SELECT memberID, username, email FROM butcher_shop_members ORDER BY username');
+                $stmt = $db->query('SELECT productID, productTitle FROM butcher_shop_products ORDER BY productID');
                 while($row = $stmt->fetch()){
 
                     echo '<tr>';
-                    echo '<td>'.$row['username'].'</td>';
-                    echo '<td>'.$row['email'].'</td>';
+                    echo '<td>'.$row['productTitle'].'</td>';
                     ?>
                     <td>
-                        <a href="edit-user.php?id=<?php echo $row['memberID'];?>">Edit</a>
-                        <?php if($row['memberID'] != 1){?>
-                            | <a href="javascript:deluser('<?php echo $row['memberID'];?>','<?php echo $row['username'];?>')">Delete</a>
-                        <?php } ?>
+                        <a href="edit-product.php?id=<?php echo $row['productID'];?>">Edit</a>
+                        <a href="javascript:delproduct('<?php echo $row['productID'];?>','<?php echo $row['productTitle'];?>')">Delete</a>
                     </td>
-
                     <?php
                     echo '</tr>';
                 }
@@ -77,7 +67,7 @@ if(isset($_GET['deluser'])){
             }
             ?>
         </table>
-        <p><a href='add-user.php'>Add User</a></p>
+        <p><a href='add-product.php'>Add Product</a></p>
     </div>
 
 </body>
